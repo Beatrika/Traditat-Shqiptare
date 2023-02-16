8ce3677321d0f909ae2e640e6dd6d19af5a9793f
Yllkasahiti <120188272+Yllkasahiti@users.noreply.github.com>
2023-02-16 12:10:03 +0100

    Edit location

--- login.php
+++ login.php
@@ -19,14 +19,14 @@ if(isset($_POST['submit'])){
          $_SESSION['admin_name'] = $row['name'];
          $_SESSION['admin_email'] = $row['email'];
          $_SESSION['admin_id'] = $row['id'];
-         header('<location:project/admin_page.php');
+         header('location:project/admin_page.php');
 
       }elseif($row['user_type'] == 'user'){
 
          $_SESSION['user_name'] = $row['name'];
          $_SESSION['user_email'] = $row['email'];
          $_SESSION['user_id'] = $row['id'];
-         header('location:../index.php');
+         header('location:index.php');
       }
 
    }else{
